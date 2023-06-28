689409f7ce7bf134e69fcdda72fde7c5e9f752c3
Sam-Hanjra <samhunjra83@gmail.com>
2023-06-28 18:57:57 +0500

    campan is about to finsh

---
+++ src/components/Setting/CampanySetting.jsx
@@ -0,0 +1,534 @@
+import { Add, PlusOne } from "@mui/icons-material";
+import imageUploader from "../../Assets/imageUploader.svg";
+import door from "../../Assets/profile.jpg";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  Switch,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useRef, useState } from "react";
+import {
+  useEditSetting,
+  useFetchDataSetting,
+} from "../../utilities/ApiHooks/setting";
+import { backendURL } from "../../utilities/common";
+
+const CampanySetting = () => {
+  const { data: settingData, refetch: reFetchDataSetting } =
+    useFetchDataSetting();
+  console.log(
+    settingData?.miscPricing?.hourlyRate,
+    "settingData?.miscPricing?.hourlyRate"
+  );
+  const {
+    mutate: editFinish,
+    isLoading: LoadingForEdit,
+    isError: ErrorForEdit,
+    isSuccess: SuccessForEdit,
+  } = useEditSetting();
+  const formik = useFormik({
+    initialValues: {
+      location: settingData?.address,
+
+      miscPricing: {
+        pricingFactor: settingData?.miscPricing?.pricingFactor || 45,
+        hourlyRate: settingData?.miscPricing?.hourlyRate,
+      },
+      fabricatingPricing: {
+        oneHoleOneByTwoInchGlass:
+          settingData?.fabricatingPricing?.oneHoleOneByTwoInchGlass,
+        oneHoleThreeByEightInchGlass:
+          settingData?.fabricatingPricing?.oneHoleThreeByEightInchGlass,
+        clampCutoutOneByTwoInch:
+          settingData?.fabricatingPricing?.clampCutoutOneByTwoInch,
+        clampCutoutThreeByEightInch:
+          settingData?.fabricatingPricing?.clampCutoutThreeByEightInch,
+        hingeCutoutOneByTwoInch:
+          settingData?.fabricatingPricing?.hingeCutoutOneByTwoInch,
+        hingeCutoutThreeByEightInch:
+          settingData?.fabricatingPricing?.hingeCutoutThreeByEightInch,
+        minterOneByTwoInch: settingData?.fabricatingPricing?.minterOneByTwoInch,
+        minterThreeByEightInch:
+          settingData?.fabricatingPricing?.minterThreeByEightInch,
+        notchOneByTwoInch: settingData?.fabricatingPricing?.notchOneByTwoInch,
+        notchThreeByEightInch:
+          settingData?.fabricatingPricing?.notchThreeByEightInch,
+        outageOneByTwoInch: settingData?.fabricatingPricing?.outageOneByTwoInch,
+        outageThreeByEightInch:
+          settingData?.fabricatingPricing?.outageThreeByEightInch,
+        polishPricePerOneByTwoInch:
+          settingData?.fabricatingPricing?.polishPricePerOneByTwoInch,
+        polishPricePerThreeByEightInch:
+          settingData?.fabricatingPricing?.polishPricePerThreeByEightInch,
+      },
+    },
+    // validationSchema: validationSchema,
+    enableReinitialize: true,
+    onSubmit: (values) => {
+      handleEditSetting(values);
+      // Handle form submission here
+      console.log(values, "onclickkk");
+    },
+  });
+  console.log(formik.values, "comapny setting");
+
+  const fileInputRef = useRef(null);
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleFileUpload = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(URL.createObjectURL(file));
+  };
+  const handleEditSetting = (props) => {
+    console.log(props, "props for edit to refetch");
+
+    // console.log(id, "id2 for edit hook");
+    editFinish(props);
+  };
+  console.log(settingData, "settingDatasettingData");
+  return (
+    <form>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+        }}
+      >
+        {/* setting */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography variant="h4">Setting</Typography>
+          <Box sx={{ width: "200px" }}>
+            <Button fullWidth variant="contained" onClick={formik.handleSubmit}>
+              Update
+            </Button>
+          </Box>
+        </Box>
+        {/* profile */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 3,
+            width: "70%",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+
+              justifyContent: "space-between",
+            }}
+          >
+            <Box>
+              {" "}
+              <Typography>Profile </Typography>
+              <Typography sx={{ color: "#667085" }}>
+                This will be displayed on your profile.
+              </Typography>
+            </Box>
+
+            <Box sx={{ display: "flex", gap: 2 }}>
+              <img
+                width={"40px"}
+                height={"40px"}
+                // src={selectedImage || imageUploader}
+                src={selectedImage || `${backendURL}/${settingData?.image}`}
+                alt="Selected"
+                style={{ borderRadius: "100%" }}
+              />
+              <Box sx={{ borderRadius: "100%", display: "flex" }}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    width: "350px",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: "8px",
+                    border: "1px solid #D9D9D9",
+                    p: 3,
+                  }}
+                >
+                  <img
+                    width={"40px"}
+                    height={"40px"}
+                    src={selectedImage || imageUploader}
+                    alt="Selected"
+                  />
+                  <Button onClick={handleFileUpload}>Click to Upload</Button>
+                  <Typography variant="body2">
+                    SVG, PNG, JPG or GIF (max. 800x400px)
+                  </Typography>
+                  <input
+                    type="file"
+                    accept="image/svg+xml,image/png,image/jpeg,image/gif"
+                    style={{ display: "none" }}
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                  />
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography>Location</Typography>
+            <Box sx={{ width: "400px" }}>
+              {" "}
+              <TextField
+                fullWidth
+                size="small"
+                type="text"
+                name="location"
+                value={formik.values?.location}
+                onChange={formik.handleChange}
+              ></TextField>
+            </Box>
+          </Box>
+        </Box>
+        {/* misc and price */}
+
+        <Box
+          sx={{
+            borderTop: "1px solid gray",
+            borderBottom: "1px solid gray",
+            paddingY: 2,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+          }}
+        >
+          <Typography variant="h6"></Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Pricing factor</Typography>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+              <p className="explain">Factor to Multiply Price </p>
+              <TextField
+                type="number"
+                name="miscPricing.pricingFactor"
+                size="small"
+                value={formik.values?.miscPricing?.pricingFactor}
+                onChange={formik.handleChange}
+              />
+              <Box sx={{ display: "flex" }}>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      color="primary"
+                      // checked={isActive || false}
+                      onChange={(event) => {
+                        const newValue = event.target.checked;
+                        // handleEntryValueChange(key, "active", newValue);
+                      }}
+                    />
+                  }
+                  label={"active"}
+                />
+              </Box>
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Default Hourly rate</Typography>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+              <Typography>Hourly rates to be used for labour price</Typography>
+              <TextField
+                type="number"
+                name="miscPricing.hourlyRate"
+                size="small"
+                value={formik.values?.miscPricing?.hourlyRate}
+                onChange={formik.handleChange}
+              />
+              <FormControlLabel
+                sx={{ visibility: "hidden" }}
+                control={<Switch />}
+                label={"active"}
+              />
+            </Box>
+          </Box>
+        </Box>
+        {/* Fabrication Pricing */}
+        <Typography variant="h6">Fabrication Pricing</Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            maxHeight: "40vh",
+            overflowY: "scroll",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>1" Hole (1/2in Glass)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              type="number"
+              name="fabricatingPricing.oneHoleOneByTwoInchGlass"
+              size="small"
+              value={
+                formik.values?.fabricatingPricing?.oneHoleOneByTwoInchGlass
+              }
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>1" Hole (3/8in Glass)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              type="number"
+              name="fabricatingPricing.oneHoleThreeByEightInchGlass"
+              size="small"
+              value={
+                formik.values?.fabricatingPricing?.oneHoleThreeByEightInchGlass
+              }
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Clamp Cutout (1/2in)</Typography>
+            <TextField
+              type="number"
+              sx={{ paddingRight: 19 }}
+              name="fabricatingPricing.clampCutoutOneByTwoInch"
+              size="small"
+              value={formik.values?.fabricatingPricing?.clampCutoutOneByTwoInch}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Clamp Cutout (3/8in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              size="small"
+              type="number"
+              name="fabricatingPricing.clampCutoutThreeByEightInch"
+              value={
+                settingData?.fabricatingPricing?.clampCutoutThreeByEightInch
+              }
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Hinge Cutout (1/2in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              size="small"
+              type="number"
+              name="fabricatingPricing.hingeCutoutOneByTwoInch"
+              value={settingData?.fabricatingPricing?.hingeCutoutOneByTwoInch}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Miter (1/2in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              name="fabricatingPricing.minterOneByTwoInch"
+              size="small"
+              type="number"
+              value={settingData?.fabricatingPricing?.minterOneByTwoInch}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Miter (3/8in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              name="fabricatingPricing.minterThreeByEightInch"
+              size="small"
+              type="number"
+              value={settingData?.fabricatingPricing?.minterThreeByEightInch}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Notch (1/2in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              name="fabricatingPricing.notchOneByTwoInch"
+              size="small"
+              type="number"
+              value={settingData?.fabricatingPricing?.notchOneByTwoInch}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Notch (3/8in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              name="fabricatingPricing.notchThreeByEightInch"
+              size="small"
+              type="number"
+              value={settingData?.fabricatingPricing?.notchThreeByEightInch}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Outage (1/2in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              name="fabricatingPricing.outageOneByTwoInch"
+              size="small"
+              type="number"
+              value={settingData?.fabricatingPricing?.outageOneByTwoInch}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Outage (3/8in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              name="fabricatingPricing.outageThreeByEightInch"
+              size="small"
+              type="number"
+              value={settingData?.fabricatingPricing?.outageThreeByEightInch}
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Polish Price per Inch (1/2in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              name="fabricatingPricing.polishPricePerOneByTwoInch"
+              size="small"
+              type="number"
+              value={
+                settingData?.fabricatingPricing?.polishPricePerOneByTwoInch
+              }
+              onChange={formik.handleChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography>Polish Price per Inch (3/8in)</Typography>
+            <TextField
+              sx={{ paddingRight: 19 }}
+              name="fabricatingPricing.polishPricePerThreeByEightInch"
+              size="small"
+              type="number"
+              value={
+                settingData?.fabricatingPricing?.polishPricePerThreeByEightInch
+              }
+              onChange={formik.handleChange}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </form>
+  );
+};
+
+export default CampanySetting;
--- src/utilities/ApiHooks/setting.js
+++ src/utilities/ApiHooks/setting.js
@@ -1,14 +1,20 @@
 import { backendURL, createSlug } from "../common";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { parseJwt } from "../../components/ProtectedRoute/AuthVerify";
 
 export const useFetchDataSetting = () => {
   async function fetchData() {
     const token = localStorage.getItem("token");
+    const decodedToken = parseJwt(token);
+    console.log(decodedToken, "decodedToken for setting");
     try {
-      const response = await axios.get(`${backendURL}/`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
+      const response = await axios.get(
+        `${backendURL}/companies/${decodedToken?.company_id}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
       console.log(response, "response");
       if (response.data && response.data.code === 200) {
         return response.data.data ? response.data.data : [];
@@ -23,6 +29,36 @@ export const useFetchDataSetting = () => {
     queryKey: ["settingData"],
     queryFn: fetchData,
     enabled: true,
-    placeholderData: [],
+    placeholderData: {},
   });
+};
+
+export const useEditSetting = () => {
+  const handleEditSetting = async (updatedSetting) => {
+    console.log(updatedSetting, "updatedSetting in hooks");
+
+    const token = localStorage.getItem("token");
+
+    try {
+      const response = await axios.put(
+        `${backendURL}/campanies/${updatedSetting?.id}`,
+        {
+          location: updatedSetting.location,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      if (response.data.code === 200) {
+        return response.data.data;
+      } else {
+        throw new Error("An error occurred while updating the data.");
+      }
+    } catch (error) {
+      throw new Error("An error occurred while updating the data.");
+    }
+  };
+
+  return useMutation(handleEditSetting);
 };
