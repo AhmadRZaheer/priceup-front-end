43d9d1dbc62a00a04fa5778f26e69473189c5e84
Eng-Abdul-Rehamn <eng.mirza.rehman@gmail.com>
2023-07-10 14:08:47 +0500

    log-in-page-added

--- src/components/ProtectedRoute/AuthenticatedRoutesStaff.js
+++ src/components/ProtectedRoute/AuthenticatedRoutesStaff.js
@@ -11,12 +11,14 @@ import Defaults from "../../pages/Layouts/Defaults";
 import Estimates from "../../pages/Estimates/Estimates";
 import Settings from "../../pages/Settings/Settings";
 import Staff from "../../pages/EstimatesMobile/Staff";
+import Login from "../Login/login";
 
 const AuthenticatedRouteStaff = () => {
   return (
     <Routes>
       <Route path="/">
         <Route path="/" element={<Staff />} />
+        <Route path="/login" element={<Login />} />
       </Route>
       <Route path="*" element={<Staff />}></Route>
     </Routes>
