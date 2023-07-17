63af0d4780c47c922d74dc553afe99dab0033107
Sam-Hanjra <samhunjra83@gmail.com>
2023-07-17 21:06:17 +0500

    channel problem resolved

--- src/components/ExtimatesMoblie/channelOrClamp.jsx
+++ src/components/ExtimatesMoblie/channelOrClamp.jsx
@@ -31,6 +31,7 @@ const ChannelType = ({
   showSnackbar,
   count,
   thickness,
+  estimatesData,
 }) => {
   const [anchorEl, setAnchorEl] = useState(false);
   const [countVal, setCountVal] = useState(count || 0);
@@ -39,7 +40,7 @@ const ChannelType = ({
   const [sleeveOverCount, setSleeveOverCount] = useState(0);
   const [towelBarsCount, setTowelBarsCount] = useState(0);
   const selectedContent = useSelector(getContent);
-  console.log(selectedContent.mounting.activeType, "selectedContent12");
+  console.log(estimatesData, "estimatesData test ");
 
   // Handle functions for each item
   const handleSleeveOverCount = (count) => {
@@ -91,8 +92,6 @@ const ChannelType = ({
         sx={{
           display: "flex",
           width: "100%",
-          // justifyContent: "space-between",
-          // alignItems: "center",
           pb: 1,
         }}
       >
@@ -115,203 +114,104 @@ const ChannelType = ({
           )}
           <Typography>{title}</Typography>
         </Button>
-        {/* {![
-          "hardwareFinishes",
-          "channel",
-          "glassTreatment",
-          "glassType",
-          "addOns",
-        ].includes(type) && (
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: 2,
-              color: { md: "#000000  ", xs: "white" },
-              alignSelf: "flex-end",
-              py: 1.5,
-            }}
-          >
-            <AddCircleOutline
-              onClick={() => handleCountSet(countVal + 1)}
-              sx={{ color: "#98A2B3" }}
-            />
-            <Typography>{countVal}</Typography>
-            <RemoveCircleOutline
-              onClick={() => handleCountSet(countVal - 1)}
-              sx={{ color: "#98A2B3" }}
-            />
-          </Box>
-        )} */}
-        {/* {["glassType"].includes(type) && (
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: 2,
-              color: { md: "#000000  ", xs: "white" },
-              alignSelf: "flex-end",
-            }}
-          >
-            <TextField
-              select
-              size="small"
-              variant="outlined"
-              InputProps={{
-                style: {
-                  color: "white", // Change the color of the input text
-                },
-                inputProps: { min: 0, max: 50 },
-              }}
-              InputLabelProps={{
-                style: {
-                  color: "rgba(255, 255, 255, 0.5)", // Change the color of the placeholder text
-                },
-              }}
-              sx={{
-                border: { md: "none", xs: "2px solid #423f57" },
-                borderRadius: { md: 0, xs: 2 },
-                color: { md: "black", xs: "white" },
-                background: "#14112c",
-                width: "100%",
-                mb: 1,
-              }}
-              // style={{ width: "100%", background: "white" }}
-              value={thicknessVal}
-              onChange={(event) => handleThicknessSet(event.target.value)}
-              // onChange={formik.handleChange} ()=>handleThicknessSet()
-              // onBlur={formik.handleBlur}
-            >
-              <MenuItem
-                key="1/2"
-                // selected={thickness === "1/2"}
-                value="1/2"
-              >
-                1/2
-              </MenuItem>
-              <MenuItem
-                key="3/8"
-                // selected={thickness === "3/8"}
-                // selected={
-                //   singleDefault?.layoutData?.settings
-                //     ?.hardwareFinishes === option?._id
-                // }
-                value="3/8"
-              >
-                3/8
-              </MenuItem>
-            </TextField>
-          </Box>
-        )} */}
       </Box>
       {anchorEl ? (
         <Box
           sx={{
-            height: "80px",
+            height: "180px",
             overflowY: "scroll",
             color: { md: "#000000", xs: "white" },
             display: "flex",
+            flexDirection: "column",
             // backgroundColor: "red",
             width: "100%",
           }}
         >
-          {menuOptions?.map((item, index) => (
-            <MenuItem key={index} onClick={() => handleItemSelect(item)}>
-              <Box
-                sx={{
-                  width: "200px",
-                  borderRadius: "12px",
-                  border:
-                    item === selectedContent?.mounting?.activeType
-                      ? "2px solid blue"
-                      : "1px solid #EAECF0",
-                  boxShadow:
-                    "0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)",
-                  // border: "1px solid #EAECF0",
-                  p: 2,
-                  display: "flex",
-                  gap: 2,
-                  alignItems: "center",
-                  width: { md: "100%", xs: "95%" },
-                  justifyContent: "space-between",
-                }}
-              >
-                <Box sx={{ display: "flex", gap: 2 }}>
-                  <img
-                    width={"25px"}
-                    height={"25px"}
-                    // src={`${backendURL}/${item?.image}`}
-                    src={Logo}
-                    alt="Selected"
-                  />
-                  <Typography>{item}</Typography>
-                </Box>
-                {/* <Box>
-                  {item?.slug === "sleeve-over" && (
-                    <Box
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-around",
-                        gap: 2,
-                        color: { md: "#000000  ", xs: "white" },
-                        alignSelf: "flex-end",
-                      }}
-                    >
-                      <AddCircleOutline
-                        onClick={() =>
-                          handleSleeveOverCount(sleeveOverCount + 1)
-                        }
-                        sx={{ color: "#98A2B3" }}
-                      />
-                      <Typography>{sleeveOverCount}</Typography>
-                      <RemoveCircleOutline
-                        onClick={() =>
-                          handleSleeveOverCount(sleeveOverCount - 1)
-                        }
-                        sx={{ color: "#98A2B3" }}
-                      />
-                    </Box>
-                  )}
-                  <Box>
-                    {item?.slug === "towel-bars" && (
-                      <Box
-                        sx={{
-                          display: "flex",
-                          alignItems: "center",
-                          gap: 2,
-                          color: { md: "#000000  ", xs: "white" },
-                          alignSelf: "flex-end",
-                        }}
-                      >
-                        <AddCircleOutline
-                          onClick={() =>
-                            handleTowelBarsCount(towelBarsCount + 1)
-                          }
-                          sx={{ color: "#98A2B3" }}
-                        />
-                        <Typography>{towelBarsCount}</Typography>
-                        <RemoveCircleOutline
-                          onClick={() =>
-                            handleTowelBarsCount(towelBarsCount - 1)
-                          }
-                          sx={{ color: "#98A2B3" }}
-                        />
-                      </Box>
-                    )}
-                  </Box>
-                </Box> */}
-
+          <Box
+            sx={{
+              display: "flex",
+              width: "100%",
+              // backgroundColor: "red",
+              width: "95vw",
+            }}
+          >
+            {menuOptions?.map((item, index) => (
+              <MenuItem key={index} onClick={() => handleItemSelect(item)}>
                 <Box
                   sx={{
+                    width: "200px",
+                    borderRadius: "12px",
+                    border:
+                      item === selectedContent?.mounting?.activeType
+                        ? "2px solid blue"
+                        : "1px solid #EAECF0",
+                    boxShadow:
+                      "0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)",
+                    // border: "1px solid #EAECF0",
+                    p: 2,
                     display: "flex",
-                    flexDirection: "column",
-                    alignContent: "space-between",
+                    gap: 2,
+                    alignItems: "center",
+                    width: { md: "100%", xs: "95%" },
+                    justifyContent: "space-between",
                   }}
-                ></Box>
-              </Box>
-            </MenuItem>
-          ))}
+                >
+                  <Box sx={{ display: "flex", gap: 2 }}>
+                    <img
+                      width={"25px"}
+                      height={"25px"}
+                      // src={`${backendURL}/${item?.image}`}
+                      src={Logo}
+                      alt="Selected"
+                    />
+                    <Typography>{item}</Typography>
+                  </Box>
+                </Box>
+              </MenuItem>
+            ))}
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignContent: "space-between",
+            }}
+          >
+            {selectedContent.mounting.activeType === "clamps" && (
+              <>
+                <MenuList
+                  menuOptions={estimatesData?.wallClamp}
+                  title={"Wall Clamps"}
+                  type={"wallClamp"}
+                  showSnackbar={showSnackbar}
+                  count={selectedContent.mounting.clamps.wallClamp.count}
+                />
+                <MenuList
+                  menuOptions={estimatesData?.sleeveOver}
+                  title={"Sleeve Over"}
+                  type={"sleeveOver"}
+                  showSnackbar={showSnackbar}
+                  count={selectedContent.mounting.clamps.sleeveOver.count}
+                />
+                <MenuList
+                  menuOptions={estimatesData?.glassToGlass}
+                  title={"Glass to Glass"}
+                  type={"glassToGlass"}
+                  showSnackbar={showSnackbar}
+                  count={selectedContent.mounting.clamps.glassToGlass.count}
+                />
+              </>
+            )}
+
+            {selectedContent.mounting.activeType === "channel" && (
+              <MenuList
+                menuOptions={estimatesData?.mountingChannel}
+                title={"Channel"}
+                type={"channel"}
+                showSnackbar={showSnackbar}
+              />
+            )}
+          </Box>
         </Box>
       ) : (
         ""
