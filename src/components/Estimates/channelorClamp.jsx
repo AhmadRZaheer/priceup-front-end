7316d9bc509b54fe8ee2a18a77dc2ed59c3b3eb0
muhammadwaqas47 <muhammadwaqas3447@gmail.com>
2023-11-22 16:16:43 +0500

    clamps data error fix

--- src/components/Estimates/channelorClamp.jsx
+++ src/components/Estimates/channelorClamp.jsx
@@ -17,13 +17,7 @@ import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
 
-const ChannelTypeDesktop = ({
-  menuOptions,
-  title,
-  type,
-  showSnackbar,
-  estimatesData,
-}) => {
+const ChannelTypeDesktop = ({ menuOptions, title, type, showSnackbar }) => {
   const [anchorEl, setAnchorEl] = useState(false);
   const selectedContent = useSelector(getContent);
   const listData = useSelector(getListData);
@@ -67,7 +61,7 @@ const ChannelTypeDesktop = ({
     else showSnackbar("Please select 'hardwareFinishes' first", "warning");
   };
   const [cornerActive, setCornerActive] = useState(true);
-  return (
+   return (
     <Box>
       <Box
         sx={{
@@ -123,50 +117,49 @@ const ChannelTypeDesktop = ({
               // gap: 1
             }}
           >
-            {
-              ["Channel", "Clamps"].map((item, index) => (
-                <MenuItem
-                  sx={{ p: 0.1 }}
-                  key={index}
-                  onClick={() => handleItemSelect(item)}
+            {["Channel", "Clamps"].map((item, index) => (
+              <MenuItem
+                sx={{ p: 0.1 }}
+                key={index}
+                onClick={() => handleItemSelect(item)}
+              >
+                <Box
+                  sx={{
+                    width: "200px",
+                    borderTopLeftRadius: index === 0 ? "12px" : "0px",
+                    borderBottomLeftRadius: index === 0 ? "12px" : "0px",
+                    borderTopRightRadius: index === 0 ? "0px" : "12px",
+                    borderBottomRightRadius: index === 0 ? "0px" : "12px",
+                    color:
+                      item.toLowerCase() === selectedContent?.mountingState
+                        ? "white"
+                        : "#101828",
+                    bgcolor:
+                      item.toLowerCase() === selectedContent?.mountingState
+                        ? "#8477DA"
+                        : "",
+                    boxShadow:
+                      "0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)",
+                    padding: "10px",
+                    display: "flex",
+                    gap: 0,
+                    alignItems: "center",
+                    width: { md: "100%", xs: "95%" },
+                    justifyContent: "space-between",
+                  }}
                 >
-                  <Box
-                    sx={{
-                      width: "200px",
-                      borderTopLeftRadius: index === 0 ? "12px" : '0px',
-                      borderBottomLeftRadius: index === 0 ? "12px" : '0px',
-                      borderTopRightRadius: index === 0 ? "0px" : '12px',
-                      borderBottomRightRadius: index === 0 ? "0px" : '12px',
-                      color:
-                        item.toLowerCase() === selectedContent?.mountingState
-                          ? "white"
-                          : "#101828",
-                      bgcolor:
-                        item.toLowerCase() === selectedContent?.mountingState
-                          ? "#8477DA"
-                          : "",
-                      boxShadow:
-                        "0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)",
-                      padding: '10px',
-                      display: "flex",
-                      gap: 0,
-                      alignItems: "center",
-                      width: { md: "100%", xs: "95%" },
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Box sx={{ display: "flex", gap: 1 }}>
-                      <img
-                        width={"25px"}
-                        height={"25px"}
-                        src={Logo}
-                        alt="Selected"
-                      />
-                      <Typography>{item}</Typography>
-                    </Box>
+                  <Box sx={{ display: "flex", gap: 1 }}>
+                    <img
+                      width={"25px"}
+                      height={"25px"}
+                      src={Logo}
+                      alt="Selected"
+                    />
+                    <Typography>{item}</Typography>
                   </Box>
-                </MenuItem>
-              ))}
+                </Box>
+              </MenuItem>
+            ))}
           </Box>
           <Box
             sx={{
@@ -178,19 +171,19 @@ const ChannelTypeDesktop = ({
             {selectedContent.mountingState === "clamps" && (
               <>
                 <MenuList
-                  menuOptions={estimatesData?.wallClamp}
+                  menuOptions={listData?.wallClamp}
                   title={"Wall Clamps"}
                   type={"wallClamp"}
                   showSnackbar={showSnackbar}
                 />
                 <MenuList
-                  menuOptions={estimatesData?.sleeveOver}
+                  menuOptions={listData?.sleeveOver}
                   title={"Sleeve Over"}
                   type={"sleeveOver"}
                   showSnackbar={showSnackbar}
                 />
                 <MenuList
-                  menuOptions={estimatesData?.glassToGlass}
+                  menuOptions={listData?.glassToGlass}
                   title={"Glass to Glass"}
                   type={"glassToGlass"}
                   showSnackbar={showSnackbar}
@@ -233,61 +226,70 @@ const ChannelTypeDesktop = ({
               </MenuItem>
             )}
           </Box>
-          {
-            ["Corner Clamps"].map((item, index) => (
-              <MenuItem
-                sx={{ borderRadius: "12px", padding: '5px', width: "fit-content", mt: 1 }}
-                key={index}
-                onClick={() => handleItemSelect(item)}
+          {["Corner Clamps"].map((item, index) => (
+            <MenuItem
+              sx={{
+                borderRadius: "12px",
+                padding: "5px",
+                width: "fit-content",
+                mt: 1,
+              }}
+              key={index}
+              onClick={() => handleItemSelect(item)}
+            >
+              <Box
+                sx={{
+                  width: "fit-content",
+                  borderRadius: "12px",
+                  background:
+                    selectedContent?.cornerClamps?.cornerWallClamp?.length ||
+                    selectedContent?.cornerClamps?.cornerSleeveOver?.length ||
+                    selectedContent?.cornerClamps?.cornerGlassToGlass?.length
+                      ? "#8477DA"
+                      : "#cccc",
+                  // color:
+                  //   item.toLowerCase() === selectedContent?.mountingState
+                  //     ? "white"
+                  //     : "black",
+                  color: "white",
+                  boxShadow:
+                    "0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)",
+                  p: 2,
+                  display: "flex",
+                  gap: 2,
+                  alignItems: "center",
+                  width: { md: "100%", xs: "95%" },
+                  justifyContent: "space-between",
+                }}
               >
-                <Box
-                  sx={{
-                    width: "fit-content",
-                    borderRadius: "12px",
-                    background: selectedContent?.cornerClamps?.cornerWallClamp?.length || selectedContent?.cornerClamps?.cornerSleeveOver?.length || selectedContent?.cornerClamps?.cornerGlassToGlass?.length ? '#8477DA' : '#cccc',
-                    // color:
-                    //   item.toLowerCase() === selectedContent?.mountingState
-                    //     ? "white"
-                    //     : "black",
-                    color: 'white',
-                    boxShadow:
-                      "0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)",
-                    p: 2,
-                    display: "flex",
-                    gap: 2,
-                    alignItems: "center",
-                    width: { md: "100%", xs: "95%" },
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <Box sx={{ display: "flex", }}>
-                    {/* <img
+                <Box sx={{ display: "flex" }}>
+                  {/* <img
                       width={"25px"}
                       height={"25px"}
                       src={Logo}
                       alt="Selected"
                     /> */}
-                    <Typography>{item}</Typography>
-                  </Box>
+                  <Typography>{item}</Typography>
                 </Box>
-              </MenuItem>
-            ))}
+              </Box>
+            </MenuItem>
+          ))}
           {cornerActive && (
             <>
               <MenuList
-                menuOptions={estimatesData?.cornerWallClamp}
+                menuOptions={listData?.cornerWallClamp}
                 title={"Wall Clamps"}
                 type={"cornerWallClamp"}
                 showSnackbar={showSnackbar}
               />
               <MenuList
-                menuOptions={estimatesData?.cornerSleeveOver}
+                menuOptions={listData?.cornerSleeveOver}
                 title={"Sleeve Over"}
                 type={"cornerSleeveOver"}
                 showSnackbar={showSnackbar}
               />
               <MenuList
-                menuOptions={estimatesData?.cornerGlassToGlass}
+                menuOptions={listData?.cornerGlassToGlass}
                 title={"Glass to Glass"}
                 type={"cornerGlassToGlass"}
                 showSnackbar={showSnackbar}
