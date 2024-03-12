1a8dc8dab6d90a6618b056895e52b541403958a2
Eng-Abdul-Rehman <eng.mirza.rehman@gmail.com>
2024-03-12 16:39:30 +0500

    additional field added and stored value in redux

--- src/components/Estimates/layoutReview.jsx
+++ src/components/Estimates/layoutReview.jsx
@@ -1,5 +1,10 @@
 import React, { useMemo } from "react";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+} from "@mui/material";
 import MenuList from "./menuList";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,26 +19,23 @@ import {
   getMeasurementSide,
   getLayoutPerimeter,
   getQuoteId,
-  // setNavigation,
   setHardwarePrice,
   setGlassPrice,
   setGlassAddonsPrice,
   setFabricationPrice,
   setLaborPrice,
-  // resetState,
   selectedItem,
   setCost,
   setProfit,
   getListData,
-  getUserProfitPercentage,
   getNotifications,
-  // setNotifications,
   setHardwareAddonsPrice,
   resetNotifications,
+  setContent,
+  getAdditionalFields,
 } from "../../redux/estimateCalculations";
 import {
   useEditEstimates,
-  // useFetchDataEstimate,
 } from "../../utilities/ApiHooks/estimate";
 import Summary from "./summary";
 import ChannelTypeDesktop from "./channelorClamp";
@@ -42,6 +44,7 @@ import { useNavigate } from "react-router-dom";
 import { layoutVariants } from "../../utilities/constants";
 import { showSnackbar } from "../../redux/snackBarSlice";
 import { useSnackbar } from "notistack";
+import { SingleField } from "../ui-components/SingleFieldComponent";
 
 const LayoutReview = ({ setClientDetailOpen }) => {
   const navigate = useNavigate();
@@ -60,9 +63,11 @@ const LayoutReview = ({ setClientDetailOpen }) => {
   const selectedContent = useSelector(getContent);
   const selectedData = useSelector(selectedItem);
   const notifications = useSelector(getNotifications);
+  const addedFields = useSelector(getAdditionalFields);
+
   const { enqueueSnackbar } = useSnackbar();
   const quoteActiveState = useMemo(() => {
-    return selectedData?.settings?.variant
+    return selectedData?.settings?.variant;
     // let state = "";
     // if (updatecheck === "create") {
     //   state = selectedData?.settings?.variant;
@@ -75,7 +80,7 @@ const LayoutReview = ({ setClientDetailOpen }) => {
   const dispatch = useDispatch();
   const handleEditEstimate = () => {
     let measurementsArray = measurements;
-    if (quoteState === 'edit' && !selectedData?.layout_id) {
+    if (quoteState === "edit" && !selectedData?.layout_id) {
       let newArray = [];
       for (const key in measurementsArray) {
         const index = parseInt(key);
@@ -203,15 +208,33 @@ const LayoutReview = ({ setClientDetailOpen }) => {
         quoteState === "create"
           ? "measurements"
           : quoteState === "custom"
-            ? "custom"
-            : quoteState === "edit" && selectedData?.layout_id
-              ? "measurements"
-              : quoteState === "edit" && !selectedData?.layout_id
-                ? "custom"
-                : "existing"
+          ? "custom"
+          : quoteState === "edit" && selectedData?.layout_id
+          ? "measurements"
+          : quoteState === "edit" && !selectedData?.layout_id
+          ? "custom"
+          : "existing"
       )
     );
   };
+
+  const handleAddField = () => {
+    const newData = [
+      ...addedFields,
+      {
+        label: "",
+        cost: 0,
+      },
+    ];
+
+    dispatch(
+      setContent({
+        type: "additionalFields",
+        item: newData,
+      })
+    );
+  };
+
   useEffect(() => {
     const prices = calculateTotal(
       selectedContent,
@@ -257,9 +280,11 @@ const LayoutReview = ({ setClientDetailOpen }) => {
   }, []);
 
   useEffect(() => {
-    console.log('mount');
+    console.log("mount");
     Object.entries(notifications).forEach(([key, value]) => {
-      if (['glassAddonsNotAvailable', 'hardwareAddonsNotAvailable'].includes(key)) {
+      if (
+        ["glassAddonsNotAvailable", "hardwareAddonsNotAvailable"].includes(key)
+      ) {
         value?.forEach((item) => {
           if (item.status) {
             enqueueSnackbar(item.message, {
@@ -276,7 +301,7 @@ const LayoutReview = ({ setClientDetailOpen }) => {
       }
     });
     return () => {
-      console.log('unmount');
+      console.log("unmount");
       dispatch(resetNotifications());
     };
   }, []);
@@ -318,8 +343,8 @@ const LayoutReview = ({ setClientDetailOpen }) => {
                 summaryState
                   ? setHandleEstimatesPages
                   : () => {
-                    setSummaryState(true);
-                  }
+                      setSummaryState(true);
+                    }
               }
             >
               {" "}
@@ -473,33 +498,33 @@ const LayoutReview = ({ setClientDetailOpen }) => {
                     layoutVariants.DOUBLEDOOR,
                     layoutVariants.DOUBLEBARN,
                   ].includes(quoteActiveState) && (
-                      <Box
-                        sx={{
-                          alignItems: "center",
-                          borderBottom: {
-                            sm: "2px solid #D0D5DD",
-                            xs: "2px solid #423f57",
-                          },
-                        }}
-                      >
-                        <Box sx={{ width: "100%", display: "flex" }}>
-                          <Box
-                            sx={{
-                              width: "100%",
-                              display: "flex",
-                              flexDirection: "column",
-                            }}
-                          >
-                            <ChannelTypeDesktop
-                              menuOptions={listData?.channelOrClamps}
-                              title={"Mounting"}
-                              type={"mounting"}
-                              listData={listData}
-                            />
-                          </Box>
+                    <Box
+                      sx={{
+                        alignItems: "center",
+                        borderBottom: {
+                          sm: "2px solid #D0D5DD",
+                          xs: "2px solid #423f57",
+                        },
+                      }}
+                    >
+                      <Box sx={{ width: "100%", display: "flex" }}>
+                        <Box
+                          sx={{
+                            width: "100%",
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                        >
+                          <ChannelTypeDesktop
+                            menuOptions={listData?.channelOrClamps}
+                            title={"Mounting"}
+                            type={"mounting"}
+                            listData={listData}
+                          />
                         </Box>
                       </Box>
-                    )}
+                    </Box>
+                  )}
                   <Box
                     sx={{
                       display: "flex",
@@ -579,7 +604,7 @@ const LayoutReview = ({ setClientDetailOpen }) => {
                         menuOptions={listData?.glassAddons}
                         title={"Glass Addons"}
                         type={"glassAddons"}
-                      // currentItem={selectedContent?.glassAddons}
+                        // currentItem={selectedContent?.glassAddons}
                       />
                     </Box>
                   </Box>
@@ -723,65 +748,65 @@ const LayoutReview = ({ setClientDetailOpen }) => {
                     layoutVariants.DOUBLEDOOR,
                     layoutVariants.DOUBLEBARN,
                   ].includes(quoteActiveState) && (
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        borderBottom: {
+                          sm: "2px solid #D0D5DD",
+                          xs: "2px solid #423f57",
+                        },
+                        paddingLeft: 3,
+                        paddingBottom: 1,
+                        color: { sm: "#000000  ", xs: "white" },
+                      }}
+                    >
+                      <Typography>Clamp Cut Out</Typography>
                       <Box
                         sx={{
                           display: "flex",
                           alignItems: "center",
-                          justifyContent: "space-between",
-                          borderBottom: {
-                            sm: "2px solid #D0D5DD",
-                            xs: "2px solid #423f57",
-                          },
-                          paddingLeft: 3,
-                          paddingBottom: 1,
-                          color: { sm: "#000000  ", xs: "white" },
+                          gap: 2,
+                          width: "120px",
+                          padddingY: 4,
                         }}
                       >
-                        <Typography>Clamp Cut Out</Typography>
-                        <Box
-                          sx={{
-                            display: "flex",
-                            alignItems: "center",
-                            gap: 2,
-                            width: "120px",
-                            padddingY: 4,
+                        <TextField
+                          type="number"
+                          InputProps={{
+                            style: {
+                              color: "black",
+                              borderRadius: 10,
+                              border: "1px solid #cccccc",
+                              backgroundColor: "white",
+                            },
+                            inputProps: { min: 0 },
                           }}
-                        >
-                          <TextField
-                            type="number"
-                            InputProps={{
-                              style: {
-                                color: "black",
-                                borderRadius: 10,
-                                border: "1px solid #cccccc",
-                                backgroundColor: "white",
-                              },
-                              inputProps: { min: 0 },
-                            }}
-                            InputLabelProps={{
-                              style: {
-                                color: "rgba(255, 255, 255, 0.5)",
-                              },
-                            }}
-                            sx={{
-                              color: { sm: "black", xs: "white" },
-                              width: "100%",
-                            }}
-                            variant="outlined"
-                            size="small"
-                            value={selectedContent.clampCut}
-                            onChange={(event) =>
-                              dispatch(
-                                setInputContent({
-                                  type: "clampCut",
-                                  value: event.target.value,
-                                })
-                              )
-                            }
-                          />
-                        </Box>
+                          InputLabelProps={{
+                            style: {
+                              color: "rgba(255, 255, 255, 0.5)",
+                            },
+                          }}
+                          sx={{
+                            color: { sm: "black", xs: "white" },
+                            width: "100%",
+                          }}
+                          variant="outlined"
+                          size="small"
+                          value={selectedContent.clampCut}
+                          onChange={(event) =>
+                            dispatch(
+                              setInputContent({
+                                type: "clampCut",
+                                value: event.target.value,
+                              })
+                            )
+                          }
+                        />
                       </Box>
-                    )}
+                    </Box>
+                  )}
                   <Box
                     sx={{
                       display: "flex",
@@ -1130,6 +1155,26 @@ const LayoutReview = ({ setClientDetailOpen }) => {
                       />
                     </Box>
                   </Box>
+                  {/* additional Fields */}
+                  <Typography variant="h5" sx={{color: {md: "black", xs: "white"}}}>Additonal Fields</Typography>
+                  {addedFields &&
+                    addedFields.map((item, index) => {
+                      return <SingleField item={item} index={index} />;
+                    })}
+                  <Button
+                    onClick={handleAddField}
+                    sx={{
+                      width: "fit-content",
+                      textTransform: "capitalize",
+                      backgroundColor: "#8477da",
+                      "&:hover": {
+                        backgroundColor: "#8477da",
+                      },
+                    }}
+                    variant="contained"
+                  >
+                    Add Additional Field
+                  </Button>
                 </Box>
               </Box>
             ) : (
@@ -1213,8 +1258,8 @@ const LayoutReview = ({ setClientDetailOpen }) => {
                     summaryState
                       ? setHandleEstimatesPages
                       : () => {
-                        setSummaryState(true);
-                      }
+                          setSummaryState(true);
+                        }
                   }
                   sx={{
                     boxShadow: "0px 1px 2px rgba(16, 24, 40, 0.05)",
---
+++ src/components/ui-components/SingleFieldComponent.jsx
@@ -0,0 +1,161 @@
+import { Delete, Done, Edit } from "@mui/icons-material";
+import { Box, IconButton, TextField, Tooltip, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import {
+  getAdditionalFields,
+  setContent,
+} from "../../redux/estimateCalculations";
+import { useDispatch, useSelector } from "react-redux";
+
+export const SingleField = ({ item, index }) => {
+  const dispatch = useDispatch();
+  const addedFields = useSelector(getAdditionalFields);
+  const [AddedValue, setAddedValue] = useState({
+    label: item.label,
+    cost: item.cost,
+  });
+  const [isEditField, setisEditField] = useState(false);
+
+  const handleSaveField = () => {
+    const updatedFields = [...addedFields];
+    updatedFields[index] = {
+      label: AddedValue.label,
+      cost: AddedValue.cost,
+    };
+    dispatch(setContent({ type: "additionalFields", item: updatedFields }));
+    setisEditField(true);
+  };
+  const handleEditField = () => {
+    setisEditField(false);
+  };
+
+  const handleLabelChange = (value) => {
+    setAddedValue((prevState) => ({
+      ...prevState,
+      label: value,
+    }));
+  };
+
+  const handleCostChange = (value) => {
+    setAddedValue((prevState) => ({
+      ...prevState,
+      cost: Number(value),
+    }));
+  };
+
+  const handleDeleteField = () => {
+    const updatedFields = addedFields.filter((field, i) => i !== index);
+    dispatch(setContent({ type: "additionalFields", item: updatedFields }));
+  };
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        borderBottom: {
+          sm: "2px solid #D0D5DD",
+          xs: "2px solid #423f57",
+        },
+        paddingLeft: 3,
+        paddingBottom: 1,
+        color: { sm: "#000000  ", xs: "white" },
+      }}
+    >
+      <Box
+        sx={{
+          width: "120px",
+        }}
+      >
+        <Typography>Label</Typography>
+        <TextField
+          disabled={isEditField}
+          placeholder="label"
+          InputProps={{
+            style: {
+              color: "black",
+              borderRadius: 10,
+              border: "1px solid #cccccc",
+              backgroundColor: "white",
+            },
+            inputProps: { min: 0 },
+          }}
+          InputLabelProps={{
+            style: {
+              color: "rgba(255, 255, 255, 0.5)",
+            },
+          }}
+          sx={{
+            color: { sm: "black", xs: "white" },
+            width: "100%",
+          }}
+          variant="outlined"
+          size="small"
+          value={AddedValue.label}
+          onChange={(e) => handleLabelChange(e.target.value)}
+        />
+      </Box>
+      <Box
+        sx={{
+          width: "120px",
+        }}
+      >
+        <Typography>Cost</Typography>
+        <TextField
+          disabled={isEditField}
+          placeholder="cost"
+          type="number"
+          InputProps={{
+            style: {
+              color: "black",
+              borderRadius: 10,
+              border: "1px solid #cccccc",
+              backgroundColor: "white",
+            },
+            inputProps: { min: 0 },
+          }}
+          InputLabelProps={{
+            style: {
+              color: "rgba(255, 255, 255, 0.5)",
+            },
+          }}
+          sx={{
+            color: { sm: "black", xs: "white" },
+            width: "100%",
+          }}
+          variant="outlined"
+          size="small"
+          value={AddedValue.cost}
+          onChange={(e) => handleCostChange(e.target.value)}
+        />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: 0.2,
+          mt: 2.4,
+        }}
+      >
+        <Tooltip title={"Delete"} placement="top">
+          <IconButton onClick={handleDeleteField}>
+            <Delete sx={{color: {md: "black", xs: "white"}}} />
+          </IconButton>
+        </Tooltip>
+        {isEditField ? (
+          <Tooltip title={"Edit"} placement="top">
+            <IconButton onClick={handleEditField}>
+              <Edit sx={{color: {md: "black", xs: "white"}}} />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title={"Save"} placement="top">
+            <IconButton onClick={handleSaveField}>
+              <Done sx={{color: {md: "black", xs: "white"}}} />
+            </IconButton>
+          </Tooltip>
+        )}
+      </Box>
+    </Box>
+  );
+};
