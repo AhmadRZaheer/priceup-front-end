15b6a42f20f4c0c1f692a8ad76e3f7080cedbf00
Muhammad Waqas <muhammadwaqas3447@gmail.com>
2024-04-18 15:32:16 +0500

    test download pdf

--- src/pages/PDFPreview/index.js
+++ src/pages/PDFPreview/index.js
@@ -1,19 +1,28 @@
-import { PDFViewer } from "@react-pdf/renderer";
+import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
 import PDFFile from "../../components/PDFFile";
 import { Box, Typography } from "@mui/material";
 
 const PDFPreview = () => {
   return (
-    <Box sx={{display:'flex',width:'100%',justifyContent:'center'}}>
-    <Box sx={{flexGrow:1,background:'red'}}>
+    <Box sx={{ display: "flex", width: "100%", justifyContent: "center" }}>
+      <Box sx={{ flexGrow: 1, background: "red" }}>
         <Typography>Div 1</Typography>
-    </Box>
-    <PDFViewer width={'60%'}>
-      <PDFFile />
-    </PDFViewer>
-    <Box sx={{flexGrow:1,background:'green'}}>
-    <Typography>Div 2</Typography>
-    </Box>
+      </Box>
+      <PDFViewer width={"60%"}>
+        <PDFFile />
+      </PDFViewer>
+      <Box sx={{ flexGrow: 1, background: "green" }}>
+        <Typography>Div 2</Typography>
+        <PDFDownloadLink document={<PDFFile />} filename="FORM">
+          {({ loading }) =>
+            loading ? (
+              <button>Loading Document...</button>
+            ) : (
+              <button>Download</button>
+            )
+          }
+        </PDFDownloadLink>
+      </Box>
     </Box>
   );
 };
