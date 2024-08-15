088c048b41c2e5c2b55459d9618edd37dc938970
Eng-Abdul-Rehman <eng.mirza.rehman@gmail.com>
2024-08-15 17:03:48 +0500

    new changes

--- src/components/Projects/existingList.jsx
+++ src/components/Projects/existingList.jsx
@@ -79,7 +79,6 @@ export default function ExistingList() {
     }
   }, [projectsList, search]);
 
-<<<<<<< HEAD
   const handleDeleteProject = () => {
     deleteProject({ apiRoute: `${routePrefix}/${deleteRecord}` });
     setDeleteModalOpen(false);
@@ -101,22 +100,6 @@ export default function ExistingList() {
   const debouncedRefetch = useCallback(
     debounce(() => {
       if (page === 1) {
-=======
-    const handleDeleteProject = () => {
-        deleteProject({ apiRoute: `${routePrefix}/${deleteRecord}` });
-        setDeleteModalOpen(false);
-    };
-    const handleViewDetail = (item) => {
-        console.log('view project detail', item);
-        navigate(`/projects/${item?._id}`)
-    };
-
-    const handleCreateProject = () => {
-        console.log('create project');
-        navigate("/projects/create")
-    };
-    useEffect(() => {
->>>>>>> efb363d40802311a7521400251c0aa841057cb37
         refetchProjectsList();
       } else {
         setPage(1);
@@ -191,7 +174,6 @@ export default function ExistingList() {
     <Box
       sx={
         {
-<<<<<<< HEAD
           // position: 'fixed', top: 50, bottom: 10,
           // left: 5, right: 5
         }
@@ -266,225 +248,6 @@ export default function ExistingList() {
           }}
         >
           <CircularProgress sx={{ color: "#8477DA" }} />
-=======
-            _id: '66a20158f92a3cada4e82604',
-            creatorData: { image: null, name: 'test creator' },
-            customerData: { image: null, name: 'test customer' },
-            updatedAt: "2024-07-25T12:57:17.641Z",
-        },
-        {
-            _id: '66a20158f92a3cada4e82604',
-            creatorData: { image: null, name: 'test creator' },
-            customerData: { image: null, name: 'test customer' },
-            updatedAt: "2024-07-25T12:57:17.641Z",
-        },
-        {
-            _id: '66a20158f92a3cada4e82604',
-            creatorData: { image: null, name: 'test creator' },
-            customerData: { image: null, name: 'test customer' },
-            updatedAt: "2024-07-25T12:57:17.641Z",
-        },
-        {
-            _id: '66a20158f92a3cada4e82604',
-            creatorData: { image: null, name: 'test creator' },
-            customerData: { image: null, name: 'test customer' },
-            updatedAt: "2024-07-25T12:57:17.641Z",
-        },
-        {
-            _id: '66a20158f92a3cada4e82604',
-            creatorData: { image: null, name: 'test creator' },
-            customerData: { image: null, name: 'test customer' },
-            updatedAt: "2024-07-25T12:57:17.641Z",
-        },
-        {
-            _id: '66a20158f92a3cada4e82604',
-            creatorData: { image: null, name: 'test creator' },
-            customerData: { image: null, name: 'test customer' },
-            updatedAt: "2024-07-25T12:57:17.641Z",
-        },
-        {
-            _id: '66a20158f92a3cada4e82604',
-            creatorData: { image: null, name: 'test creator' },
-            customerData: { image: null, name: 'test customer' },
-            updatedAt: "2024-07-25T12:57:17.641Z",
-        },
-    ]
-    return (
-        <Box sx={{
-            // position: 'fixed', top: 50, bottom: 10,
-            // left: 5, right: 5
-        }}>
-            {/* <Box
-                sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    paddingY: 2,
-                    paddingX: isMobile ? 0.5 : 2,
-                }}
-            >
-                <Typography sx={{ fontSize: isMobile ? 18 : 20, fontWeight: "bold", color: "#101828" }}>
-                    Projects
-                </Typography> */}
-                {/* Search input field */}
-                {/* <TextField
-                    placeholder="Search by Customer / Project Name"
-                    value={search}
-                    variant="standard"
-                    onChange={(e) => handleChange(e)}
-                    sx={{
-                        mb: 2,
-                        ".MuiInputBase-root:after": {
-                            border: "1px solid #8477DA",
-                        },
-                        width: isMobile ? '150px' : 'auto'
-                    }}
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <Search sx={{ color: "#8477DA" }} />
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-                <IconButton
-                    onClick={handleCreateProject}
-                    disabled={projectsListFetching}
-                    sx={{
-                        backgroundColor: "#8477DA",
-                        color: "white",
-                        "&:hover": { backgroundColor: "#8477DA" },
-                        borderRadius: 1,
-                        padding: 1,
-                        textTransform: "capitalize",
-                        fontSize: 16,
-                        height: 35,
-                    }}
-                >
-                    <Add sx={{ width: 24 }} />
-                    Add
-                </IconButton>
-            </Box> */}
-
-            {isLoading ? (
-                <Box
-                    sx={{
-                        width: 40,
-                        m: "auto",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        maxHeight: "70vh",
-                        minHeight: "20vh",
-                    }}
-                >
-                    <CircularProgress sx={{ color: "#8477DA" }} />
-                </Box>
-            ) : filteredData?.length === 0 && !projectsListFetching ? (
-                <Typography sx={{ color: "#667085", p: 2, textAlign: "center" }}>
-                    No Projects Found
-                </Typography>
-            ) : (
-                <Box >
-                    {isMobile ?
-                        (filteredData?.map((item) => <Box
-                            key={item._id}
-                            sx={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                                paddingY: 2,
-                                borderBottom: "1px solid rgba(102, 112, 133, 0.5)",
-                            }}
-                        >
-                            <Box sx={{ display: "flex", gap: 1 }}>
-                                <Box
-                                    sx={{
-                                        width: 40,
-                                        height: 40,
-                                        borderRadius: "100%",
-                                        overflow: "hidden",
-                                    }}
-                                >
-                                    <DefaultImage
-                                        image={item?.creatorData?.image}
-                                        name={item?.creatorData?.name}
-                                    />
-                                </Box>
-
-                                <Box>
-                                    <Box sx={{ display: "flex", gap: 0.6 }}>
-                                        <Typography className={classes.overflowText}>
-                                            {item?.creatorData?.name}
-                                        </Typography>
-                                        <Typography
-                                            sx={{ fontSize: 16, fontWeight: "Medium" }}
-                                        >
-                                            {" "}
-                                            - Creator
-                                        </Typography>
-                                    </Box>
-                                    <Box sx={{ display: "flex", gap: 0.6 }}>
-                                        <Typography sx={{ fontSize: 14 }}>
-                                            {item?.customerData?.name}
-                                        </Typography>
-                                        <Typography sx={{ fontSize: 14 }}>
-                                            {" "}
-                                            - Customer
-                                        </Typography>
-                                    </Box>
-                                </Box>
-                            </Box>
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    textAlign: "center",
-                                    width: 100,
-                                    alignItems: "center"
-                                }}
-                            >
-                                <ActionsDropdown item={item} actions={dropdownActions} />
-                                <Typography
-                                    sx={{ fontWeight: "Medium", fontSize: 12 }}
-                                >
-                                    {new Date(item?.updatedAt).toDateString()}
-                                </Typography>
-                            </Box>
-                        </Box>))
-                        : (<DataGrid
-                            loading={projectsListFetching}
-                            style={{
-                                border: "none",
-                               }}
-                            getRowId={(row) => row._id}
-                            rows={filteredData}
-                            columns={ProjectsColumns(dropdownActions)}
-                            page={page}
-                            pageSize={itemsPerPage}
-                            rowCount={
-                                projectsList?.totalRecords ? projectsList?.totalRecords : 0
-                            }
-                            rowHeight={70.75}
-                            sx={{ width: "100%" }}
-                            disableColumnMenu
-                            hideFooter
-                        />)}
-                    <Pagination
-                        totalRecords={projectsList?.totalRecords ? projectsList?.totalRecords : 0}
-                        itemsPerPage={itemsPerPage}
-                        page={page}
-                        setPage={setPage}
-                    />
-                    <DeleteModal
-                        open={deleteModalOpen}
-                        close={() => {
-                            setDeleteModalOpen(false);
-                        }}
-                        isLoading={LoadingForDelete}
-                        handleDelete={handleDeleteProject}
-                    />
-                </Box>
-            )}
->>>>>>> efb363d40802311a7521400251c0aa841057cb37
         </Box>
       ) : filteredData?.length === 0 && !projectsListFetching ? (
         <Typography sx={{ color: "#667085", p: 2, textAlign: "center" }}>
